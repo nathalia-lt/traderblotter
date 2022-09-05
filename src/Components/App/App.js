4593e82162f8e58fa08b6aa2394344e4849f5c98
nathalia <nathaliatroina@gmail.com>
2022-09-05 12:50:18 -0400

    new data

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,32 @@
 import { Route, Routes } from 'react-router-dom';
 import key from '../../Key'
+import { useEffect, useState } from 'react';
+import data from '../../data'
 
 function App() {
 
-  let url = ''
 
-  var myHeaders = new Headers();
-myHeaders.append("apikey", key);
 
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-  headers: myHeaders
-};
 
-fetch("https://api.apilayer.com/fixer/latest", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+//   let url = "https://api.apilayer.com/fixer/latest?base=usd"
+
+//   let [data, setData] = useState([])
+
+//   var myHeaders = new Headers();
+// myHeaders.append("apikey", key);
+
+// var requestOptions = {
+//   method: 'GET',
+//   redirect: 'follow',
+//   headers: myHeaders
+// };
+// useEffect(() => { //useEffect does not let fetch run none stop
+// fetch(url, requestOptions)
+//   .then(response => response.json()) //response is what we get from fetch and we turn it into json that is readeable
+//   .then(result => console.log(result)) //result of the response.json //here we need to keep that data that response.json is given
+//   .catch(error => console.log('error', error));
+// },[])
+  
 
 
 
