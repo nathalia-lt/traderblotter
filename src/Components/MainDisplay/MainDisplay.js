8c67afb398f791e9dae539d11339766445a5b358
nathalia <nathaliatroina@gmail.com>
2022-09-05 13:41:02 -0400

    start currency card

--- src/Components/MainDisplay/MainDisplay.js
+++ src/Components/MainDisplay/MainDisplay.js
@@ -1,14 +1,26 @@
 
 
 
-export default function MainDisplay({ data }){
+export default function MainDisplay({ data }) {
 
     let first = data.rates[0]
     console.log(first)
 
 
 
-    return(
-        <div>oii</div>
+    return (
+        <div className='mainDisplayContainer'>
+            <div className='currencyCard' >
+                <div className='currencyName'> BRL</div>
+                <div className='currencyPrice'>
+                    <div className='buy side' >hey there</div>
+                    <div className='sell side' > hey you</div>
+                </div>
+
+                <div className='currencyFluctuation'> 1.67899 </div>
+
+            </div>
+
+        </div>
     )
 }
