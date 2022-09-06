1812d91733050bc8ce392742d9c17d8e6e5f1ce4
nathalia <nathaliatroina@gmail.com>
2022-09-06 09:32:15 -0400

    main display variables

--- src/Components/MainDisplay/MainDisplay.js
+++ src/Components/MainDisplay/MainDisplay.js
@@ -4,20 +4,26 @@
 export default function MainDisplay({ data }) {
 
     let first = data.rates[0]
-    console.log(first)
+    
+    let symbol = first.symbol
 
+    let price = first.price
+    
 
 
     return (
         <div className='mainDisplayContainer'>
             <div className='currencyCard' >
-                <div className='currencyName'> BRL</div>
+                <div className='currencyName'> {symbol}</div>
                 <div className='currencyPrice'>
-                    <div className='buy side' >hey there</div>
-                    <div className='sell side' > hey you</div>
+                    <div className='buy side' > {price} </div>
+                    <div className='sell side' > {price} </div>
                 </div>
 
-                <div className='currencyFluctuation'> 1.67899 </div>
+                <div className='currencyFluctuation'> 
+                <div className='fluctuation'> H: 1.67899 </div>
+                <div className='fluctuation'> L: 1.67899 </div>
+                </div>
 
             </div>
 
